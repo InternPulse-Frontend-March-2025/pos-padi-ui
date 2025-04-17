ade2121bd23fa4e4a3fab54052f111de1fb699aa
zorichDev <ffranzor@yahoo.com>
2025-04-17 08:44:31 +0100

    Agent on boarding page

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import HomePage from "./pages/HomePage";
 import Settings from "./pages/Settings";
 import Dashboard from "./pages/Dashboard";
 import Layout from "./pages/Layout";
-import Emailtemplate  from "./pages/Emailtemplate";
 import OTPVerificationPage from "./pages/OTPVerificationPage";
+import AgentOnboardingPage from "./pages/AgentOnboardingPage";
 
 function App() {
 
@@ -15,8 +15,8 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path='dashboard' element={<Dashboard />} />
           <Route path='settings' element={<Settings />} />
-          <Route path='Emailtemplate' element={<Emailtemplate />} />
           <Route path='OTPVerificationpage' element={<OTPVerificationPage />} />
+          <Route path='AgentOnboardingpage' element={<AgentOnboardingPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,24 @@
 import { Outlet, Link } from "react-router-dom";
+import { Box, Flex, Button } from '@chakra-ui/react';
 
 function Layout() {
   return (
-    <div>
-   
-    </div>
+    <Box>
+      {/* Optional Navbar or Sidebar can go here */}
+      <Flex p={4} gap={4} bg="gray.100">
+        <Button as={Link} to="/">Home</Button>
+        <Button as={Link} to="/dashboard">Dashboard</Button>
+        <Button as={Link} to="/settings">Settings</Button>
+        <Button as={Link} to="/Emailtemplate">Email Template</Button>
+        <Button as={Link} to="/OTPVerificationpage">OTP Page</Button>
+        <Button as={Link} to="/AgentOnboardingpage">Onboarding</Button>
+      </Flex>
+
+      {/* Main content area */}
+      <Box p={6}>
+        <Outlet />
+      </Box>
+    </Box>
   );
 }
 
